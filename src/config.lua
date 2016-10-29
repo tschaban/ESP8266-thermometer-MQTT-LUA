d52ea2f9c984c49898850f5dbb27afa9ea234fd2
adrian <dev@adrian.czabanowski.com>
2016-10-29 19:06:25 +0200

    config set as module

--- src/config.lua
+++ src/config.lua
@@ -3,29 +3,26 @@
 -- LICENCE: http://opensource.org/licenses/MIT
 -- 2016-10-11 tschaban https://github.com/tschaban
 
+local module = {}
 
--- WiFi Configuration
-wifiSSID = ""
-wifiPassword = ""
+module.ID = node.chipid()
 
--- MQTT Broker configuration
-mqttBroker = ""
-mqttPort = 1883
-mqttUser = ""
-mqttPass = ""
+module.WIFI_SSID = "SSID"
+module.WIFI_PASSWORD = "password"
 
--- This parameter defines ID of a device. ID is used in MQTT topic
-deviceID = "0"
+module.MQTT_HOST = "mqtt.example.com"
+module.MQTT_PORT = 1883
+module.MQTT_USER = "user"
+module.MQTT_PASSWORD = "password"
+module.MQTT_TOPIC = "/sensor/"
 
--- Sensor
-temperatureInterval = 5000
-temperaturePIN = 2
-currentTemperature = 0
-temperatureCorrection = -2
+module.INTERVAL = 5000
 
--- LCD
-sdaPIN = 5 -- GPIO14 / D5
-sclPIN = 6 -- GPIO12 / D6
+module.TEMP_SENSOR = 2
+module.TEMP_CORRECTION = 0
 
--- Script version
-version = "0.2"
+module.SDA = 5 -- GPIO14 / D5
+module.SCL = 6 -- GPIO12 / D6
+
+return module
+
